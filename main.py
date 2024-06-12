05dbc37d27d7bbf06dc8cb9eb76d99232c612b19
Aqib Ali <info.aqib.co.in@gmail.com>
2024-06-12 11:28:02 +0530

    Added popups to review details and empty fields and save details in data.txt

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from tkinter import *
-def close_window():
-    window.destroy()
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def write_to_file():
+    website = website_entry.get()
+    email = username_entry.get()
+    password = password_entry.get()
+
+    if website == "" or password == "":
+        messagebox.showwarning(title="Oops", message="Please dont leave any field empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                  f"\nPassword: {password} \n Is it OK to save?")
+        if is_ok:
+            with open("data.txt", mode="a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+            website_entry.delete(0,END)
+            password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=70, pady=50)
@@ -28,7 +45,7 @@ password_label.grid(row=3, column=0, pady=5)
 gen_pass_button = Button(text="Generate Password", relief="groove")
 gen_pass_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=44, relief="groove")
+add_button = Button(text="Add", width=44, relief="groove", command=write_to_file)
 add_button.grid(row=4, column=1, columnspan=2, pady=5)
 
 # Entries
@@ -38,11 +55,9 @@ website_entry.focus()
 
 username_entry = Entry(width=52)
 username_entry.grid(row=2, column=1, columnspan=2)
-username_entry.insert(0, "jacksonsam786@gmail.com" )
+username_entry.insert(0, "dummy@xyz.com" )
 
 password_entry = Entry(width=33)
 password_entry.grid(row=3, column=1)
-
-window.after(10000, close_window)
 
 window.mainloop()
