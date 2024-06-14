508fd4e463f1e294adc76ad4899a403b768c9797
Aqib Ali <info.aqib.co.in@gmail.com>
2024-06-14 21:33:33 +0530

    Added JSON for saving the data, search funtionality and error handling

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# password-generator-manager
-GUI password generator and manager using pyhton and tkinter
+# GUI password generator and manager using pyhton and tkinter
+# This project is created using python 3.12 and pycharm IDE
+
+**Steps to run the project**
+1. Install python 3.12
+2. Install module pyperclip
+3. Run the project in the IDE
+
+# Usage Guide
+![Screenshot](./Screenshot.png)
+
+## Saving a password
+- Enter the website name.
+- Enter the E-mail/Username
+- Enter password or click on generate password
+- Click Add
+- Confirm details
+- Password will saved and copied to the clipboard
+
+## Searching a saved password
+- Enter the website name and click search
+- If the search is successful, the password will be copied to the clipboard
+
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -21,25 +22,62 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def write_to_file():
-    website = website_entry.get()
+    website = website_entry.get().title()
     email = username_entry.get()
     password = password_entry.get()
-
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
     if website == "" or password == "" or email == "":
         messagebox.showwarning(title="Oops", message="Please dont leave any field empty.")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
                                                               f"\nPassword: {password} \n Is it OK to save?")
         if is_ok:
-            with open("data.txt", mode="a") as file:
-                file.write(f"{website} | {email} | {password}\n")
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+            try:
+                with open("data.json", mode="r") as file:
+                    data = json.load(file)
+            except (FileNotFoundError, json.decoder.JSONDecodeError):
+                with open("data.json", 'w') as file:
+                    json.dump(new_data, file, indent=4)
+            else:
+                data.update(new_data)
+                with open("data.json", "w") as file:
+                    json.dump(data, file, indent=4)
+            finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+                username_entry.delete(0, END)
+
+# --------------------------FIND PASSWORD------------------------------ #
+
+
+def find_password():
+    website = website_entry.get().title()
+    if website == "":
+        messagebox.showwarning(title="Oops", message="Please type a website to search.")
+    else:
+        try:
+            with open("data.json") as file:
+                data = json.load(file)
+            email = data[website]["email"]
+            password = data[website]["password"]
+        except (FileNotFoundError, json.JSONDecodeError):
+            messagebox.showinfo(title="OPPS", message="You have not saved any passwords yet")
+        except KeyError:
+            messagebox.showinfo(title="OOPS", message="Credentials Not Found")
+        else:
+            messagebox.showinfo(title=f"Details Found for {website}", message=f"Email: {email}\n Password: {password}\n"
+                                                                              f"Password copied to clipboard.")
+            pyperclip.copy(password)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -68,13 +106,17 @@ gen_pass_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=44, relief="groove", command=write_to_file)
 add_button.grid(row=4, column=1, columnspan=2, pady=5)
 
+search_button = Button(text="Search", relief="groove", width=14, command=find_password)
+search_button.grid(row=1, column=2)
+
 # Entries
-website_entry = Entry(width=52)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=33)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
 username_entry = Entry(width=52)
 username_entry.grid(row=2, column=1, columnspan=2)
+# If write an email in the username entry, uncomment the below line and insert your email at the place of dummy@xyz.com
 # username_entry.insert(0, "dummy@xyz.com" )
 
 password_entry = Entry(width=33)
